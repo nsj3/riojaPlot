a998a098773e0108393ac20bedfb8a33f3c7057b
nsj3 <Stephen.Juggins@ncl.ac.uk>
2022-10-30 15:34:56 +0000

    V 9

--- R/zzz.r
+++ R/zzz.r
@@ -1,6 +1,6 @@
 .onAttach <- function(lib, pkg)  {
    line1 <- paste("This is riojaPlot ", utils::packageDescription("riojaPlot", fields="Version"))
-   line2 <- "New function riojaPlot replaces strat.plot for plotting stratigraphic diagrams\nSee ?riojaPlot and vignette(\"riojaPlot\") or vignette(\"riojaPlotPDF\") for details."
+   line2 <- "riojaPlot replaces strat.plot for plotting stratigraphic diagrams\nSee ?riojaPlot and browseVignettes(\"riojaPlot\")."
    packageStartupMessage(paste(line1, line2, sep="\n"), appendLF = TRUE)
 }
 
